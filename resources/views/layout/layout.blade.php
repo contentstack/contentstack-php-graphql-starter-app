23086485db7f64a542d3af01db97dcbca881422a
sunil-lakshman <104969541+sunil-lakshman@users.noreply.github.com>
2023-05-12 12:27:54 +0530

    Added Css files in https environment (#5)
    * Added Css files in https environment
    
    * Removed unused env variable

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -11,7 +11,7 @@
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer">
    <title>Contentstack-PHP-Starter-App</title>
-   <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
+   <link rel="stylesheet" href="{{ secure_asset('css/main.css') }}" />
 </head>
   <body>
     <div id="root">
